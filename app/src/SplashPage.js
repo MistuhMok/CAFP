fe468760b4acd5990d949b63d3d6c3de13b59e14
Alex Mok <mokalexander00@gmail.com>
2020-01-31 00:01:47 -0500

    feat: search for stores on splash page"

--- app/src/SplashPage.js
+++ app/src/SplashPage.js
@@ -5,18 +5,29 @@ import { withRouter } from 'react-router-dom';
 // import SearchBar from "./SearchBar";
 
 class SplashPage extends React.Component {
-  onSubmit() {}
+  state = { address: '' };
+
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   render() {
     // const { drizzle, drizzleState, props } = this.props;
     return (
       <Segment placeholder>
-        <Form>
+        <Form
+          onSubmit={() =>
+            this.props.history.push(`/store/${this.state.address}`)
+          }
+        >
           <Form.Input
             icon="building"
             iconPosition="left"
             label="Store Address"
             placeholder="Address"
+            name="address"
+            value={this.state.address}
+            onChange={this.handleInputChange}
           />
           <Button content="Search" primary />
         </Form>
